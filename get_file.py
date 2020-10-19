843d93d7be22c300d8ead0b9fa345a395c2b0aec
caprize <iskan20022@mail.ru>
2020-10-19 23:02:54 +0300

    stable url

--- get_file.py
+++ get_file.py
@@ -8,9 +8,24 @@ import urllib
 import time
 from threading import Thread
 import subprocess
+from bs4 import BeautifulSoup
+import urllib.parse
 
 def update():
-    urllib.request.urlretrieve(url, "sched.xlsx")  # For Python 3
+    with open("schedule_page.html", "w") as f:
+        sched_page = requests.get(url)
+        f.write(sched_page.text)
+        sched_page = str(sched_page.text)
+        sched_page = BeautifulSoup(sched_page,"lxml")
+
+        link = sched_page.find_all("a")[132]
+        link = str(link['href'])
+        linkloc = link[46::]
+        linkloc = urllib.parse.quote(linkloc)
+        link = link[0:46]
+        f.close
+    link += linkloc
+    urllib.request.urlretrieve(link, "sched.xlsx")  # For Python 3
     subprocess.Popen(['python3', "work_with_file.py"])
 
 now = (datetime.datetime.now())
